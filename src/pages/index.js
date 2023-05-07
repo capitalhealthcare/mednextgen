829e1c68fca189ed8fd976681c87edcc2ac3b8ec
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-05-07 07:04:39 +0500

    set home with fashion two as default

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,56 @@
 import { useSelector } from "react-redux";
+import { LayoutTwo } from "../layouts";
 import { getProducts } from "../lib/product";
-import { LayoutOne } from "../layouts";
-import { HeroSliderOne } from "../components/HeroSlider";
-import { BannerOne, BannerTwo } from "../components/Banner";
-import { ProductTab } from "../components/ProductTab";
-import { ProductSliderOne } from "../components/ProductSlider";
-import { TestimonialOne } from "../components/Testimonial";
-import { IconBoxOne } from "../components/IconBox";
+import { BannerTwo, BannerThree } from "../components/Banner";
+import { ProductTabTwo } from "../components/ProductTab";
+import {
+  ProductSliderThree,
+  ProductSliderFour
+} from "../components/ProductSlider";
+import { BrandLogoOne } from "../components/BrandLogo";
+import { HeroSliderTwo } from "../components/HeroSlider";
 
-import heroSliderOneData from "../data/hero-sliders/hero-slider-one.json";
-import testimonialOneData from "../data/testimonials/testimonial-one.json";
+import heroSliderTwoData from "../data/hero-sliders/hero-slider-two.json";
+import brandLogoData from "../data/brand-logo/brand-logo-one.json";
 
-const FashionOne = () => {
+const FashionTwo = () => {
   const { products } = useSelector((state) => state.product);
   const featuredProducts = getProducts(products, "fashion", "featured", 8);
   const newProducts = getProducts(products, "fashion", "new", 8);
-  const bestSellerProducts = getProducts(products, "fashion", "popular", 8);
+  const bestSellerProducts = getProducts(products, "fashion", "popular", 10);
   const saleProducts = getProducts(products, "fashion", "sale", 8);
 
   return (
-    <LayoutOne>
+    <LayoutTwo navPositionClass="justify-content-start">
       {/* hero slider */}
-      <HeroSliderOne heroSliderData={heroSliderOneData} />
-      {/* double banner */}
-      <BannerTwo />
+      <HeroSliderTwo heroSliderData={heroSliderTwoData} />
       {/* tab product */}
-      <ProductTab
+      <ProductTabTwo
         title="Exclusive Products"
         newProducts={newProducts}
         bestSellerProducts={bestSellerProducts}
         featuredProducts={featuredProducts}
         saleProducts={saleProducts}
       />
-      {/* single banner */}
-      <BannerOne />
+      {/* banner */}
+      <BannerTwo />
       {/* product slider */}
-      <ProductSliderOne title="Featured Products" products={featuredProducts} />
-      {/* testimonial */}
-      <TestimonialOne testimonialData={testimonialOneData} />
-      {/* icon box */}
-      <IconBoxOne />
-    </LayoutOne>
+      <ProductSliderThree
+        title="Deal Of The Day"
+        products={saleProducts}
+        dateTime="July 07, 2024 12:12:00"
+      />
+      {/* banner */}
+      <BannerThree />
+      {/* product slider */}
+      <ProductSliderFour
+        title="Bestseller Products"
+        products={bestSellerProducts}
+      />
+      {/* brand logo */}
+      <BrandLogoOne title="Our Brands" brandLogoData={brandLogoData} />
+    </LayoutTwo>
   );
 };
 
-export default FashionOne;
+export default FashionTwo;
